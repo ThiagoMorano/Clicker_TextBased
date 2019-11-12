747ce95fdaaafb953c394701174f700f9f5ad5ad
ThiagoMorano <thiago.morano159@gmail.com>
2019-11-12 12:40:06 +0100

    Include introduction

--- Clicker_TextBased/Clicker_TextBased/Program.cs
+++ Clicker_TextBased/Clicker_TextBased/Program.cs
@@ -11,12 +11,22 @@ namespace Clicker_TextBased
     {
         static void Main(string[] args)
         {
-            //Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
             Console.SetWindowSize(140, 32);
+            Console.CursorVisible = false;
+
+            Console.WriteLine("The world needs to be hacked.");
+            Console.WriteLine("Generates lines of code with [SpaceBar]");
+            Console.WriteLine("Purchase items to help you with hacking the world.");
+            Console.WriteLine("Good luck and happy hacking!");
+            Console.WriteLine("Press any key to start...");
+
+            Console.ReadKey(true);
+
+            Console.Clear();
+    
+
             Game game = new Game();
             game.Init();
-
-            Console.CursorVisible = false;
 
             const double FPS = 24;
             double timeUntilNextFrame = 0.0d;
